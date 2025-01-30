09975815d3b7ee7e23125c46c33ba6d0458c9f27
JoaoBiel11 <joao.pavanete.tds24@gmail.com>
2025-01-30 15:28:21 -0300

    Mudei o main e o Ataque

--- Java_ex1/src/Ataque.java
+++ Java_ex1/src/Ataque.java
@@ -1,6 +1,6 @@
 public class Ataque {
     private int id;
-    private String tipo; // "fisico" ou "magico"
+    private String tipo;
     private int dano;
 
     public Ataque(int id, String tipo, int dano) {
@@ -11,18 +11,18 @@ public class Ataque {
 
     public int calcularDanoPersonagem(Personagem personagem) {
         if (this.tipo.equals("fisico")) {
-            return this.dano * personagem.getForca();  // Acesso através do getter
+            return this.dano * personagem.getForca();
         } else if (this.tipo.equals("magico")) {
-            return this.dano * personagem.getInteligencia();  // Acesso através do getter
+            return this.dano * personagem.getInteligencia();
         }
         return 0;
     }
 
     public int calcularDanoInimigo(Inimigo inimigo) {
-        return this.dano + inimigo.getDano();  // Acesso através do getter
+        return this.dano + inimigo.getDano();
     }
 
-    // Getters
+
     public String getTipo() {
         return tipo;
     }
--- Java_ex1/src/Main.java
+++ Java_ex1/src/Main.java
@@ -1,27 +1,81 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // Criando os personagens
-        Personagem harry = new Personagem(1, "Harry", 1000, 10, 200);
-        Personagem hobbit = new Personagem(2, "Hobbit", 500, 200, 10);
+        Scanner scanner = new Scanner(System.in);
 
-        // Criando os inimigos
+        System.out.println("Vamos criar seu personagem!");
+
+
+        System.out.print("Digite o nome do seu personagem: ");
+        String nome = scanner.nextLine();
+
+
+        System.out.print("Digite os pontos de vida (HP) do seu personagem: ");
+        int hp = scanner.nextInt();
+
+
+        System.out.print("Digite a força do seu personagem: ");
+        int forca = scanner.nextInt();
+
+
+        System.out.print("Digite a inteligência do seu personagem: ");
+        int inteligencia = scanner.nextInt();
+
+
+        Personagem personagem = new Personagem(1, nome, hp, forca, inteligencia);
+
+
+
         Inimigo dragao = new Inimigo(1, "O Dragão", 3000, 300);
         Inimigo gigante = new Inimigo(2, "O Gigante", 5000, 200);
 
-        // Criando os ataques
+        System.out.println("Escolha o inimigo a enfrentar:" + "\n1- Dragão\n" + "2- Gigante");
+        int escolha = scanner.nextInt();
+
+        Inimigo inimigoEscolhido = (escolha == 1) ? dragao : gigante;
+
+
         Ataque ataqueFisico = new Ataque(1, "fisico", 20);
         Ataque ataqueMagico = new Ataque(2, "magico", 50);
+
+
 
         // Simulando a batalha
         System.out.println("Início do combate:\n");
 
-        // Harry ataca o Gigante com ataque mágico
-        harry.atacar(gigante, ataqueMagico);
+        System.out.println("\nEscolha o tipo de ataque:");
+        System.out.println("1 - Ataque Físico");
+        System.out.println("2 - Ataque Mágico");
+        int escolhaAtaque = scanner.nextInt();
 
-        // Hobbit ataca o Dragão com ataque físico
-        hobbit.atacar(dragao, ataqueFisico);
 
-        // O Dragão ataca o Hobbit com ataque físico
-        dragao.atacar(hobbit, ataqueFisico);
+        Ataque ataqueEscolhido = (escolhaAtaque == 1) ? ataqueFisico : ataqueMagico;
+
+
+        while (personagem.getHp() > 0 && inimigoEscolhido.getHp() > 0) {
+            // Atacar o inimigo
+            System.out.println("\n" + personagem.getNome() + " ataca " + inimigoEscolhido.getNome() + " com um ataque " + ataqueEscolhido.getTipo());
+            personagem.atacar(inimigoEscolhido, ataqueEscolhido);
+            System.out.println(inimigoEscolhido.getNome() + " agora tem " + inimigoEscolhido.getHp() + " pontos de vida.");
+
+
+            if (inimigoEscolhido.getHp() <= 0) {
+                System.out.println(inimigoEscolhido.getNome() + " foi derrotado!");
+                break;
+            }
+            System.out.println(inimigoEscolhido.getNome() + " ataca " + personagem.getNome() + " com ataque físico.\n");
+            inimigoEscolhido.atacar(personagem, ataqueFisico);  // Usando um ataque físico do inimigo
+            System.out.println(personagem.getNome() + " agora tem " + personagem.getHp() + " pontos de vida.\n");
+
+
+            if (personagem.getHp() <= 0) {
+                System.out.println(personagem.getNome() + " foi derrotado!");
+                break;
+            }
+
+            }
+
     }
-}
+
+    }
