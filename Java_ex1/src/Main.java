b5fa55f59c5ce501cc5ba0ac11aa5a7ed76d3795
GabrielCardoso76 <torresgbriel8@gmail.com>
2025-01-30 14:48:20 -0300

    Criando seu proprio personagem

--- Java_ex1/src/Main.java
+++ Java_ex1/src/Main.java
@@ -1,27 +1,42 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         // Criando os personagens
-        Personagem harry = new Personagem(1, "Harry", 1000, 10, 200);
-        Personagem hobbit = new Personagem(2, "Hobbit", 500, 200, 10);
+        Scanner tec = new Scanner(System.in);
+
+        System.out.println("Digite o nome do seu personagem!");
+        String nome1 = tec.nextLine();
+        System.out.println("Digite o Vida que seu personagem vai ter");
+        System.out.println("HP maximo 5000");
+        int hp1 = tec.nextInt();
+        System.out.println("Digite a Força do seu personagem");
+        int forca1 = tec.nextInt();
+        System.out.println("Digite a inteligencia do seu personagem");
+        int inteligencia1 = tec.nextInt();
+        Personagem p1 = new Personagem(0, nome1, hp1, forca1, inteligencia1);
 
         // Criando os inimigos
         Inimigo dragao = new Inimigo(1, "O Dragão", 3000, 300);
-        Inimigo gigante = new Inimigo(2, "O Gigante", 5000, 200);
 
         // Criando os ataques
         Ataque ataqueFisico = new Ataque(1, "fisico", 20);
         Ataque ataqueMagico = new Ataque(2, "magico", 50);
-
+        boolean batalha = true;
+        while (batalha){
+            batalha = false;
+            batalha = true;
+        }
         // Simulando a batalha
         System.out.println("Início do combate:\n");
 
         // Harry ataca o Gigante com ataque mágico
-        harry.atacar(gigante, ataqueMagico);
+        p1.atacar(dragao, ataqueMagico);
 
         // Hobbit ataca o Dragão com ataque físico
-        hobbit.atacar(dragao, ataqueFisico);
+        p1.atacar(dragao, ataqueFisico);
 
         // O Dragão ataca o Hobbit com ataque físico
-        dragao.atacar(hobbit, ataqueFisico);
+        dragao.atacar(p1, ataqueFisico);
     }
 }
